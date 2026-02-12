55ccba977d6d5b88c789534b7d3967081476c601
rafapcdev <raf.pc.dev@gmail.com>
2026-02-11 22:35:28 -0300

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,8 +2,7 @@ import { HERO_CONTENT } from "../constant/index"
 import { SepareteContent } from "./Utility"
 import imgProfile from "../assets/profile.png"
 import {motion} from "motion/react"
-import cvRafael from "../assets/Rafael_Costa_Engenheiro_Software.pdf";
-
+import cvRafael from "../assets/Rafael_Costa_Engenheiro_Software_DevOps.pdf";
 
 function Hero() {
     return (
@@ -14,26 +13,28 @@ function Hero() {
                     <motion.h1 
                     initial={{x:-100, opacity:0}}
                     whileInView={{x:0, opacity:1}}
-                    transition={{duration:1.2, delay:0}}
+                    transition={{duration:0.7, delay:0}}
                     className="text-6xl font-thin tracking-tight">{HERO_CONTENT.name}</motion.h1>
                     <motion.span 
                     initial={{x:-100, opacity:0}}
                     whileInView={{x:0, opacity:1}}
-                    transition={{duration:1.2, delay:.5}}
+                    transition={{duration:0.9, delay:.3}}
                     className="text-3xl font-semibold tracking-tight bg-gradient-to-r from-fuchsia-500 to-cyan-500 bg-clip-text text-transparent">{HERO_CONTENT.title}</motion.span>
                     <motion.p 
                     initial={{x:-100, opacity:0}}
                     whileInView={{x:0, opacity:1}}
+                    transition={{duration:1.1, delay:1}}
+                    className="font-light tracking-wider max-w-xl">{HERO_CONTENT.brief}</motion.p>
+                    <motion.p
+                    initial={{x:-100, opacity:0}}
+                    whileInView={{x:0, opacity:1}}
                     transition={{duration:1.5, delay:1}}
-                    className="font-light whitespace-pre-line tracking-wider max-w-xl">{HERO_CONTENT.brief}
+                    className="font-light whitespace-pre-line tracking-wider max-w-xl">
                     </motion.p>
-
                     <motion.a
                     initial={{x:-100, opacity:0}}
                     whileInView={{x:0, opacity:1}}
                     transition={{duration:1.5, delay:1}}
-                    className="font-light tracking-wider max-w-xl">{HERO_CONTENT.brief}</motion.p>
-                    <a
                     className="rounded-2xl p-4 border border-neutral-700 text-2xl font-semibold tracking-tight bg-gradient-to-r from-fuchsia-500 to-cyan-500 bg-clip-text text-transparent"
                     href={cvRafael}
                     target="_blank"
